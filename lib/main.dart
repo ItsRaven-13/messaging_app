e8aa1acfd1961f20c2e0e2c2c93f767ef6951ec5
Uriel Barrera Sánchez <barrerasanchezu@gmail.com>
2025-10-02 20:34:16 -0600

    feat(Firebase):Se integra Firebase al proyecto Flutter version android.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:messaging_app/app/app.dart';
 import 'package:messaging_app/core/providers/connectivity_provider.dart';
+import 'package:messaging_app/firebase_options.dart';
 import 'package:provider/provider.dart';
 
-// Asegúrate de tener esta pantalla creada
-
-void main(List<String> args) {
+Future<void> main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MultiProvider(
       providers: [
