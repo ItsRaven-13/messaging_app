0c02d74f460ff63a2d2454b71253dce1a63a686a
Uriel Barrera Sánchez <barrerasanchezu@gmail.com>
2025-11-30 22:21:23 -0600

    refactor(hasInternet): Se mejora el network util para mejorar la comprobación de internet con el paquete internet_connection_checker_plus

--- lib/core/utils/network_utils.dart
+++ lib/core/utils/network_utils.dart
@@ -1,18 +1,12 @@
-import 'dart:io';
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 class NetworkUtils {
-  /// Verifica si hay conectividad (WiFi/datos).
   static Future<bool> hasInternetConnection() async {
     final result = await Connectivity().checkConnectivity();
     if (result == ConnectivityResult.none) return false;
 
-    // Verificación más robusta (ping a un dominio conocido)
-    try {
-      final lookup = await InternetAddress.lookup('google.com');
-      return lookup.isNotEmpty && lookup.first.rawAddress.isNotEmpty;
-    } catch (_) {
-      return false;
-    }
+    final bool isConnected = await InternetConnection().hasInternetAccess;
+    return isConnected;
   }
 }
